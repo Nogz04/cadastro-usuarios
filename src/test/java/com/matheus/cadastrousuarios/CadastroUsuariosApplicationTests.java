b7cc8b82fcaa9dea6c2d385a2eb5bf0d3ff8b646
Matheus Nogueira <matheus.nalbuquerque@ufn.edu.br>
2025-07-25 16:51:46 -0300

    projeto java spring boot de cadastro de usuarios

---
+++ src/test/java/com/matheus/cadastrousuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.cadastrousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
